ed97f0cd3b7b3f7ccf36895ed3af6f9989b95dc2
Manikandan-Raju <67756241+Manikandan-Raju@users.noreply.github.com>
2020-11-25 18:13:36 +0530

    Update can_send.py

--- raspberrypi_code/python/can_send.py
+++ raspberrypi_code/python/can_send.py
@@ -3,15 +3,28 @@ import os
 import can
 import time
 
+
+
+
+
+count =0
 bus0 = can.interface.Bus(bustype='socketcan_native', channel='can0')
+msg = can.Message(arbitration_id=0x700,data=[count,0x01,0x02, 0x03, 0x04, 0x05,0x06, 0x12],extended_id=True)
+        
+
+
 
 try:
+    task = bus0.send_periodic(msg,1,duration=5)
     while True:
-        msg = can.Message(arbitration_id=0x7de,data=[count,0x01,0x02, 0x03, 0x04, 0x05,0x06, 0x12],extended_id=True)
-        bus0.send(msg)
-        print("{}: {}".format(msg.arbitration_id, msg.data))
-        time.sleep(1)
-
+        #msg = can.Message(arbitration_id=0x700,data=[count,0x01,0x02, 0x03, 0x04, 0x05,0x06, 0x12],extended_id=True)
+        msg.data[0] = count
+        task.modify_data(msg)
+        count += 1
+        if count <= 5:
+                print("{}: {}".format(msg.arbitration_id, msg.data))
+        time.sleep(0.5)
+        
 
 
 except KeyboardInterrupt:
